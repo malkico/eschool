663df67d39a6e2c85b2e6d1397203a36bdded43f
sk <dev.bairouk@gmail.com>
2022-06-30 10:29:11 +0200

    fix

--- Frontend.php
+++ Frontend.php
@@ -1,5 +1,5 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT'].'/seolyzer-e7bdf29c38c13de794e8d1df64aa9105.php.php';
+require $_SERVER['DOCUMENT_ROOT'].'/seolyzer-e7bdf29c38c13de794e8d1df64aa9105.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- include.php
+++ include.php
@@ -1,3 +1,3 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT'].'/seolyzer-e7bdf29c38c13de794e8d1df64aa9105.php.php';
+require $_SERVER['DOCUMENT_ROOT'].'/seolyzer-e7bdf29c38c13de794e8d1df64aa9105.php';
 ?>
